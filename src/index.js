ac46e0b9d259a84baaff55044cb8268db1d3fa24
Roy Portas <royportas@gmail.com>
2017-12-04 17:32:01 +1000

    Migrated to main site

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+// import registerServiceWorker from './registerServiceWorker';
 import '../node_modules/bulma/css/bulma.css';
 import './index.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+// registerServiceWorker();
